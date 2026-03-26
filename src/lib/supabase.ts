d2fd2317f905349a3268f27813d8c7f9726844ee
Ragen11 <fadhilizzati11@gmail.com>
2026-03-26 18:51:56 +0700

    Update Kode

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sucfuzsxrlcyzlyagwjp.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1Y2Z1enN4cmxjeXpseWFnd2pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyOTM2MDAsImV4cCI6MjA4OTg2OTYwMH0.ZusM8V0RbSJvXsq1aTH3FvMpOpHRiOc6rYGGHzYNsjI";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
