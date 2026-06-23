0e3535ee7483b7f040719c38ece2eadb1c33fc9d
Ragen11 <fadhilizzati11@gmail.com>
2026-06-23 18:05:31 +0700

    update main

---
+++ src/lib/axiosAuth.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { supabase } from "./supabase";
+
+axios.interceptors.request.use(async (config) => {
+  const { data: { session } } = await supabase.auth.getSession();
+  if (session?.access_token) {
+    config.headers.Authorization = `Bearer ${session.access_token}`;
+  }
+  return config;
+});
